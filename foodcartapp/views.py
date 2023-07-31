963cfc2b820ea75307695097c6cbecdec3faa8ce
Andrey7777777777 <117460052+Andrey7777777777@users.noreply.github.com>
2023-07-31 19:58:18 +0300

    Add models for order data.

---
+++ foodcartapp/views.py
@@ -0,0 +1,80 @@
+import json
+
+from django.http import JsonResponse
+from django.templatetags.static import static
+
+
+from .models import Product, Order, OrderItem
+
+
+def banners_list_api(request):
+    # FIXME move data to db?
+    return JsonResponse([
+        {
+            'title': 'Burger',
+            'src': static('burger.jpg'),
+            'text': 'Tasty Burger at your door step',
+        },
+        {
+            'title': 'Spices',
+            'src': static('food.jpg'),
+            'text': 'All Cuisines',
+        },
+        {
+            'title': 'New York',
+            'src': static('tasty.jpg'),
+            'text': 'Food is incomplete without a tasty dessert',
+        }
+    ], safe=False, json_dumps_params={
+        'ensure_ascii': False,
+        'indent': 4,
+    })
+
+
+def product_list_api(request):
+    products = Product.objects.select_related('category').available()
+
+    dumped_products = []
+    for product in products:
+        dumped_product = {
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+            'special_status': product.special_status,
+            'description': product.description,
+            'category': {
+                'id': product.category.id,
+                'name': product.category.name,
+            } if product.category else None,
+            'image': product.image.url,
+            'restaurant': {
+                'id': product.id,
+                'name': product.name,
+            }
+        }
+        dumped_products.append(dumped_product)
+    return JsonResponse(dumped_products, safe=False, json_dumps_params={
+        'ensure_ascii': False,
+        'indent': 4,
+    })
+
+
+def register_order(request):
+    try:
+        order_info = json.loads(request.body.decode())
+        order = Order.objects.create(
+            name=order_info['firstname'],
+            family_name=order_info['lastname'],
+            phone=order_info['phonenumber'],
+            adress=order_info['address']
+        )
+        for products in order_info['products']:
+            order_item = OrderItem.objects.create(
+                product_id=products['product'],
+                quantity=products['quantity'],
+                order=order
+            )
+        return JsonResponse({'order_id': order.id}, status=201)
+
+    except ValueError:
+        return JsonResponse({'error': 'Invalid JSON'}, status=400)
